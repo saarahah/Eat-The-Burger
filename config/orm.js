8c042dea4c65bb8bd42b5d44f55a70695e019021
saarahah <sarahlferguson07@gmail.com>
2020-07-19 20:21:54 -0400

    replaced keywords

--- config/orm.js
+++ config/orm.js
@@ -29,8 +29,8 @@ function objToSql(ob) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+      // e.g. {burger_name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {devoured: true} => ["devoured=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -70,7 +70,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  // An example of objColVals would be {name: panther, devoured: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -103,5 +103,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('guac burger');
+INSERT INTO burgers (burger_name) VALUES ('dumb burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('good burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('regular burg');
