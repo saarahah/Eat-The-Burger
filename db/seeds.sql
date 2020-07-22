e4cca527fa11fab6723097179f8a130aecd95985
saarahah <sarahlferguson07@gmail.com>
2020-07-21 22:35:50 -0400

    sci fi burger styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('guac burger');
-INSERT INTO burgers (burger_name) VALUES ('dumb burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('good burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Ghost Pepper In The Shell Burger');
+INSERT INTO burgers (burger_name) VALUES ('Total Recauliflower Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Burginator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('2001: A Space Onion Ring Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden Burger', true);
 INSERT INTO burgers (burger_name) VALUES ('regular burg');
