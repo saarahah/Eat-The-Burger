755225c0850b44d3f3e41404405f43add91bd690
saarahah <sarahlferguson07@gmail.com>
2020-07-21 22:46:43 -0400

    sci fi

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ INSERT INTO burgers (burger_name) VALUES ('Ghost Pepper In The Shell Burger');
 INSERT INTO burgers (burger_name) VALUES ('Total Recauliflower Burger');
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Burginator', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('2001: A Space Onion Ring Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden Burger', true);
-INSERT INTO burgers (burger_name) VALUES ('regular burg');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tomatrix', true);
+INSERT INTO burgers (burger_name) VALUES ('');
